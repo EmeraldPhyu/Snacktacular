1802dad351255d6b1f93cb06666ffc517abaa172
Emerald <phyuhninhtway@gmail.com>
2024-12-05 18:23:59 +0800

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Emerald on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
